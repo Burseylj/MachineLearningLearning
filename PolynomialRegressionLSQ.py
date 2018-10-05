52d223d57f84da39d04c10bdbfdda9cbfc80f637
burseylj <burseylj@users.noreply.github.com>
2018-10-04 23:02:23 -0400

    refactored polynomial regression as a library

--- PolynomialRegressionLSQ.py
+++ PolynomialRegressionLSQ.py
@@ -3,23 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# generating data
 
-# Random data
-points = 30
-order =1
-data = np.random.random((points,2)) # plus 1 to get constant term 
-print data
-
-# We are solving for coeff = ( transpose(A)*A)^-1 * transpose(A)*y
-# where A = [1 x_1 x^2_1 ... x^n_1]
-#           [1                    ]
-#                   ...
-#           [1 x_m  ...      x^n_m]
-#           y = [y1 ...ym]
-#          and coeff is the coefficient vector
-
-#generate A from inputs
 def getM(x,points,order):
     M = np.zeros(shape=(points,order+1))
     for i in xrange(points):
@@ -27,24 +11,49 @@ def getM(x,points,order):
             M[i,j] = x[i]**j
     return M
 
-x = data[:,0]
-A = getM(x,points,order+1)
-y = data[:,1].T
 
-coeff = np.linalg.inv(A.T.dot(A)).dot(A.T).dot(y)
+def bestFitCoeff(x,y,order):
+    points = x.shape[0]
+    # We are solving for coeff = ( transpose(A)*A)^-1 * transpose(A)*y
+    # where A = [1 x_1 x^2_1 ... x^n_1]
+    #           [1                    ]
+    #                   ...
+    #           [1 x_m  ...      x^n_m]
+    #           y = [y1 ...ym]
+    #          and coeff is the coefficient vector
+
+    #generate A from inputs
+    
+    A = getM(x,points,order)
+
+    coeff = np.linalg.inv(A.T.dot(A)).dot(A.T).dot(y)
+    return coeff
+
 
 #we define a function for the best fit polynomial
-def bestFit(val):
+def bestFit(val,coeff):
     output = 0
-    for i in xrange(order+1):
+    for i in xrange(coeff.size):
         output += coeff[i]*val**i
     return output
 
-# Plot data, regression line
-xx = np.linspace(0, 1, 50)
-F = bestFit(xx)
+if __name__ == "__main__":
+    # Random data
+    points = 100
+    order =1
+    data = np.random.random((points,2))
+    x = data[:,0]
+    y = data[:,1].T
+    y = x+ (y-0.5)/5
 
-plt.figure(1)
-plt.plot(xx, F, color='b')
-plt.scatter(data[:,0], data[:,1], color='r')
-plt.show()
+    coeff = bestFitCoeff(x,y,order)
+    
+    # Plot data, regression line
+    xx = np.linspace(0, 1, 50)
+    F = bestFit(xx,coeff)
+
+    plt.figure(1)
+    plt.plot(xx, F, color='b')
+    plt.scatter(x, y, color='r')
+    plt.show()
+
